941ade60b7aea5771d81b2a6e08da30d604287f6
Sam Chandra <sam.chandra415@gmail.com>
2026-04-23 19:57:10 +1200

    Update policy

--- src/FlowinglyImport.Api/Program.cs
+++ src/FlowinglyImport.Api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
@@ -38,7 +38,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseExceptionHandler();
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("Frontend");
 
